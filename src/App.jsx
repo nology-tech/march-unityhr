f643bb73af780ac654d5bf9e00b4c4a5db8c0e19
moni0012 <moni.kumari@db.com>
2022-05-20 16:54:43 +0300

    Setup clean project and installed Router Dom

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return <div className="App"></div>;
+};
+
+export default App;

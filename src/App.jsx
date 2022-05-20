242359c211ebf50d001df13fe8787ec6cb06f1d1
moni0012 <moni.kumari@db.com>
2022-05-20 17:21:01 +0300

    added h1 and deleted unwanted html

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import "./App.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>UnityHR</h1>
+    </div>
+  );
 };
 
 export default App;

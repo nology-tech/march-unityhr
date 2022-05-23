76e062403bc685b2e873c9819694d8b7d74bc3da
miari <maryna.boroday@gmail.com>
2022-05-23 15:08:56 +0200

    update to pipeline setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 
-import { initializeApp } from "firebase/app";
+/*import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -23,7 +23,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
+*/
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {

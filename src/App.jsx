0e51873d5b9c4114e6c4e379bc3a60e59103456b
miari <maryna.boroday@gmail.com>
2022-05-23 15:14:38 +0200

    App file changed

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 */
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (

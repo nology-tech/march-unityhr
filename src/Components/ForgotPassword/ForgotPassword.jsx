06ae4231b2b3d7449031d8dce500bbbc449bdc0f
jacintha <jacintha.h@gmail.com>
2022-05-25 17:50:48 +0200

    almost done - right side

--- src/Components/ForgotPassword/ForgotPassword.jsx
+++ src/Components/ForgotPassword/ForgotPassword.jsx
@@ -2,24 +2,35 @@ import "./ForgotPassword.scss";
 import React from "react";
 
 const ForgotPassword = () => {
-  return(
-
- 
+  return (
     <div className="forgotten-password">
-       
-      <form className="forgotten-password__box">
-        <h2 className="forgotten-password--heading">Forgotten your Password </h2>
-        <p>That's no worries. We all make mistakes from time to time. Just enter your email address below to get a link to change your password</p>
-        <label className="forgotten-password--EmailLabel" for="Email Address"></label>
-        <input className="forgotten-password--EmailInput" type="text" name="Email Address"></input>
-        <button className="forgotten-password--SubmitButton">submit</button>
-       
+      <div className="forgotten-password__AuthenticationDesign">
+        <h2>Streamlined task management</h2>
+      </div>
+      <form className="forgotten-password-form">
+        <h2 className="forgotten-password-form__heading">
+          Forgotten your Password{" "}
+        </h2>
+        <p className="forgotten-password-form__caption">
+          That's no worries. We all make mistakes from time to time. Just enter
+          your email address below to get a link to change your password
+        </p>
+        <label
+          className="forgotten-password-form__EmailLabel"
+          htmlFor="Email Address"
+        >
+          Email Address
+        </label>
+        <input
+          className="forgotten-password-form__EmailInput"
+          type="text"
+          name="Email Address"
+        ></input>
+        <button className="forgotten-password-form__SubmitButton">
+          Submit
+        </button>
       </form>
-      <section>
-          <h2>Streamlined task management</h2>
-       
-      </section>
     </div>
-  )
+  );
 };
 export default ForgotPassword;

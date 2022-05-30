45749e0ed1cd80809f557e6a5eaaa81a289d2f4e
moni0012 <moni.kumari@db.com>
2022-05-30 17:49:32 +0300

    styling for front page

--- src/component/LoginPage/LoginPage.jsx
+++ src/component/LoginPage/LoginPage.jsx
@@ -5,45 +5,44 @@ const LoginPage = ({ emailInput, passwordInput, onChange }) => {
   return (
     <section className="login-container">
       <AuthenticationPageLeft />
-      <div>
+
+      <form className="login-form">
         <h1 className="login-container__title">Login UnityHR</h1>
-        <form className="login-form">
-          <label className="login-form__email">Email Address</label>
+        <label className="login-form__email">Email Address</label>
+        <input
+          type="text"
+          placeholder="steve.walker@unity.io"
+          emailValue={emailInput}
+          className="login-form__email--input"
+        />
+
+        <br />
+
+        <label className="login-form__password">Password</label>
+        <input
+          type="text"
+          id="user-password"
+          placeholder="••••••••••"
+          passValue={passwordInput}
+          className="login-form__password--input"
+        />
+        <section className="login-form__checkbox">
           <input
-            type="text"
-            placeholder="steve.walker@unity.io"
-            emailValue={emailInput}
-            className="login-form__email--input"
+            type="checkbox"
+            onChange={onChange}
+            value={"checked"}
+            className="login-form__checkbox--box"
           />
+          <label className="login-form__checkbox--label">Remember Me</label>
+          <a className="login-form__checkbox--link" href="url">
+            Forgot your password?
+          </a>
+        </section>
 
-          <br />
-
-          <label className="login-form__password">Password</label>
-          <input
-            type="text"
-            id="user-password"
-            placeholder="••••••••••"
-            passValue={passwordInput}
-            className="login-form__password--input"
-          />
-          <section className="login-form__checkbox">
-            <input
-              type="checkbox"
-              onChange={onChange}
-              value={"checked"}
-              className="login-form__checkbox--box"
-            />
-            <label className="login-form__checkbox--label">Remember Me</label>
-            <a className="login-form__checkbox--link" href="url">
-              Forgot Your PassWord?
-            </a>
-          </section>
-
-          <button className="login-form__button" type="button">
-            Login
-          </button>
-        </form>
-      </div>
+        <button className="login-form__button" type="button">
+          Login
+        </button>
+      </form>
     </section>
   );
 };

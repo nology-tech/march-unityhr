d041b1d057695f3839ffbb1098df7d6c143f1d82
Blanche Jarrett <blanchejarrett@gmail.com>
2022-05-30 16:36:58 +0100

    make email and password fields mandatory

--- src/component/AuthenticationPageLeft/AuthenticationPageLeft.jsx
+++ src/component/AuthenticationPageLeft/AuthenticationPageLeft.jsx
@@ -10,8 +10,9 @@ const AuthenticationPageLeft = () => {
         </h1>
         <p>
           {" "}
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Semoer id
-          mattis ultrices sed augue pretium sagittis, cursus.{" "}
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          <br />
+          Semoer id mattis ultrices sed augue pretium sagittis, cursus.{" "}
         </p>
       </div>
     </div>

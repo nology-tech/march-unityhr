244fb3fbf451311b5677a3399a61a0b7201317e6
miari <maryna.boroday@gmail.com>
2022-05-31 15:55:07 +0200

    Ticket alt was added in Menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -7,7 +7,7 @@ import Setting from "../../assets/images/setting.png"
 const Menu = () => {
   return (
     <div className="menu-section">
-      <div className="menu-section__item menu-section__item--selected"><img src={Ticket}/><p>My Tickets</p></div>
+      <div className="menu-section__item menu-section__item--selected"><img src={Ticket} alt="Ticket"/><p>My Tickets</p></div>
       <div className="menu-section__item"><img src={Bell} alt="Bell" /><p>Notifications</p></div>
       <div className="menu-section__item"><img src={Employees} alt="Employees" /><p>Employees</p></div>
       <div className="menu-section__item"><img src={Setting} alt="Settings" /><p>Settings</p></div>

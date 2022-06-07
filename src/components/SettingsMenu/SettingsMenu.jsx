ebd433e809ca76d291559a19c2557842776fa92d
Blanche Jarrett <blanchejarrett@gmail.com>
2022-06-07 16:07:54 +0100

    copying folder to right branch

--- src/components/SettingsMenu/SettingsMenu.jsx
+++ src/components/SettingsMenu/SettingsMenu.jsx
@@ -5,8 +5,11 @@ import Display from "../../assets/images/display.png";
 import User from "../../assets/images/user.png";
 import Login from "../../assets/images/login.png";
 import Vector from "../../assets/images/Vector.png";
+import ProfileSettings from "../../containers/ProfileSettings/ProfileSettings";
+import LoginSettings from "../../containers/LoginSettings/LoginSettings";
+import DisplaySettings from "../../containers/DisplaySettings/DisplaySettings";
 
-const Settings = () => {
+const SettingsMenu = () => {
   return (
     <div className="setting-section">
       <h3 className="setting-section__heading">Settings</h3>
@@ -40,8 +43,13 @@ const Settings = () => {
           alt="Vector"
         />
       </div>
+      <div>
+        <ProfileSettings />
+        <LoginSettings />
+        <DisplaySettings />
+      </div>
     </div>
   );
 };
 
-export default Settings;
+export default SettingsMenu;

83f63c99555586028307c5be966b58ba941d6773
Soudeh Gholami <soudehschl@Soudehs-Air.fritz.box>
2022-06-09 17:19:45 +0200

    styling finished

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -3,20 +3,28 @@ import React from "react";
 
 const Notification = () => {
   return (
+    <section className="notification">
     <div className="container">
       <div className="container__notification">
         <div>arrow image</div>
-        <div className="container__notification-details">
-          UserImage
-          <div className="container__notification-details-users">
-            <div>User name</div>
-            <div>comments</div>
+        <div className="container__notification-details"> UserImage</div>
+        <div className="container__notification-details-users">
+          <div className="container__notification-details-username">
+            User name
           </div>
+          <div className="container__notification-details-comments">
+            comments
+          </div>
+        
         </div>
-        <div>time</div>
+        <div className="container__time">00:00</div>
       </div>
-      <div className="line"> </div>
+     
+     
     </div>
+    <div className="line"> </div>
+     </section>
+
   );
 };
 

6a08bde51866d23df5af58b968b9cced4d496811
jacintha <jacintha.h@gmail.com>
2022-06-20 16:50:58 +0200

    completed main function

--- src/containers/Workspace/Workspace.jsx
+++ src/containers/Workspace/Workspace.jsx
@@ -1,4 +1,4 @@
-import "./Workspace.scss"
+import "./Workspace.scss";
 import Ticket from "../../components/Ticket/Ticket";
 import DetailedTicket from "../../components/DetailedTicket/DetailedTicket";
 import React, { useState } from "react";
@@ -6,16 +6,12 @@ import GridMenu from "../../assets/images/menu-grid.png";
 import ListMenu from "../../assets/images/bullet-list.png";
 import Filter from "../../assets/images/Filter.png";
 
+const Workspace = (props) => {
+  const { color, category } = props;
 
+  const [showPopUp, setShowPopUp] = useState(false);
 
-
-const Workspace=(props)=>{
-    const {color, category } = props;
-
-
-    const [showPopUp, setShowPopUp] = useState(false);
- 
-  const ticketColorBarBlue= "#16a6f8"
+  const ticketColorBarBlue = "#16a6f8";
   const ids = [1, 2, 3, 4, 5];
 
   const togglePopUp = () => {
@@ -24,8 +20,8 @@ const Workspace=(props)=>{
 
   const workspaceTicketsJSX = ids.map((index) => (
     <div onClick={togglePopUp} key={"ticket" + index}>
-          <Ticket category="Payroll" color={ticketColorBarBlue}/>
-        </div>
+      <Ticket category="Payroll" color={ticketColorBarBlue} />
+    </div>
   ));
 
   return (
@@ -37,21 +33,34 @@ const Workspace=(props)=>{
         <div className="my-tickets-board__top__header">
           <h3>Workspace</h3>
         </div>
-        <div className="my-tickets-board__top__GridMenu"><img src={GridMenu} alt="GridMenu"></img></div>
-        <div className="my-tickets-board__top__ListMenu"><img src={ListMenu} alt="ListMenu"></img></div>
+        <div className="my-tickets-board__top__GridMenu">
+          <img src={GridMenu} alt="GridMenu"></img>
+        </div>
+        <div className="my-tickets-board__top__ListMenu">
+          <img src={ListMenu} alt="ListMenu"></img>
+        </div>
         <div className="my-tickets-board__top__filters">
-            <button  className="my-tickets-board__top__filters-button" ><img src={Filter} alt="Filter"></img>Filters</button></div>
+          {/* <button className="my-tickets-board__top__filters-button"> */}
+          <img className="" src={Filter} alt="Filter"></img>
+          {/* </button> */}
+          Filters
+        </div>
       </div>
       <div className="my-tickets-board__main">
-        <p className="my-tickets-board__main-header">Unassigned</p>
-        <p className="my-tickets-board__main-header">Pending with HR</p>
-        <p className="my-tickets-board__main-header">Pending with Employee</p>
-        <p className="my-tickets-board__main-header">Resolved</p>
-        {workspaceTicketsJSX}
+        <div className="my-tickets-board__main-titles">
+          <p className="my-tickets-board__main-header">Backlog</p>
+          <p className="my-tickets-board__main-header">Unresolved</p>
+          <p className="my-tickets-board__main-header">In Review</p>
+          <p className="my-tickets-board__main-header">Resolved</p>
+          <p className="my-tickets-board__main-header">Archive</p>
+        </div>
+        <div className="my-tickets-board__main-tickets">
+          {" "}
+          {workspaceTicketsJSX}
+        </div>
       </div>
       <div className="my-tickets-board__add-ticket">Add Ticket</div>
     </div>
   );
-    
-    };
-    export default Workspace;
+};
+export default Workspace;

d1ef29574d33efc1d8a3741cf123d583750e1526
Soudeh Gholami <soudehschl@Soudehs-Air.fritz.box>
2022-06-22 15:31:41 +0200

    deleted some component from dashboard jsx

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -1,29 +1,20 @@
 import "./Dashboard.scss";
-import React, { useState } from "react";
+import React from "react";
 import DashboardOverview from "../../components/DashboardOverview/DashboardOverview";
 import DashboardStatus from "../../components/DashboardStatus/DashboardStatus";
 
-const Dashboard=()=>{
-
-
-
-
-    const [showPopUp, setShowPopUp] = useState(false);
-
-
-    const togglePopUp = () => {
-      setShowPopUp(!showPopUp);
-    };
-  
-    
-
-    return (<div>
-     <div > <DashboardOverview/></div>
-   <div> <DashboardStatus/></div>
-  
-  
- 
-     </div>
-    );
+const Dashboard = () => {
+  return (
+    <div>
+      <div>
+        {" "}
+        <DashboardOverview />
+      </div>
+      <div>
+        {" "}
+        <DashboardStatus />
+      </div>
+    </div>
+  );
 };
 export default Dashboard;

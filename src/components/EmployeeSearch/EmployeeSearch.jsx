17c4010aaa51d0942f340a40813dc83f5a76a0a9
Eli Kh <elikh@Elis-MBP.fritz.box>
2022-06-22 12:21:20 +0200

    search function added

--- src/components/EmployeeSearch/EmployeeSearch.jsx
+++ src/components/EmployeeSearch/EmployeeSearch.jsx
@@ -1,1 +1,26 @@
-{/* <input type="text" placeholder="Search title" onInput={handleEvent} className="search-box__input"/> */}
+import React, { useState } from "react";
+import "./EmployeeSearch.scss";
+
+const Search = (props) => {
+  const { onChange } = props;
+  const [employeeSearch, setEmployeeSearch] = useState("");
+
+  const changeHandler = (e) => {
+    setEmployeeSearch(e.target.value);
+    onChange(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        className="search-employee"
+        type="text"
+        placeholder="Search employee"
+        value={employeeSearch}
+        onChange={changeHandler}
+      ></input>
+    </div>
+  );
+};
+
+export default Search;
